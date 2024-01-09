b9aa19bd7d1687fd9f124ea85e8ad55fba3b362e
Ric-De <ricdellazari@gmail.com>
2024-01-09 19:46:47 +0100

    Controllers and Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\GameController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', HomeController::class)->name('home');
+
+Route::resource('teams', TeamController::class);
+Route::resource('games', GameController::class);
