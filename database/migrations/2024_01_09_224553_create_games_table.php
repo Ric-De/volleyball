a58c7135145f73ac998449fdc886d88a80798e24
Ric-De <ricdellazari@gmail.com>
2024-01-10 00:00:52 +0100

    migrations teams and games tables

---
+++ database/migrations/2024_01_09_224553_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->time('time');
+            $table->unsignedBigInteger('yellow_team_id');
+            $table->unsignedBigInteger('blue_team_id');
+            $table->integer('yellow_score');//sets: min 0 max 2
+            $table->integer('blue_score');//sets: min 0 max 2
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
