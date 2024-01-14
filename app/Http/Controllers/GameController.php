543afa9fb4eedf4f09c7f37c9695628c1e5519a8
Ric-De <ricdellazari@gmail.com>
2024-01-14 05:54:59 +0100

    method index and index view

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Game;
 
 class GameController extends Controller
 {
@@ -11,7 +12,9 @@ class GameController extends Controller
      */
     public function index()
     {
-        //
+        //$games = Game::all();
+        $games = Game::orderBy('date', 'asc')->get(); //verify to order by date and time
+        return view("games.index", compact('games'));
     }
 
     /**
